2846cad06f73b76fd0a4cd14f144f1fe5ac53c65
MartinVeltman <martinveltman@live.nl>
2022-06-14 18:35:24 +0200

    rename, check

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,29 +1,47 @@
 import React, {useState} from "react";
-import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+import Popup from 'reactjs-popup';
 
 const BookCard = ({book: {id, title, imagePath, autor, description, review, releaseYear}}) => {
     const [open, setOpen] = useState(false);
     const closeModal = () => setOpen(false);
 
     return (
-        <div className="book" key={id}>
+        <div>
+            <div className="book" key={id}>
 
-            <div>
-                <p>{releaseYear}</p>
-            </div>
+                <div>
+                    <p>{releaseYear}</p>
+                </div>
 
-            <div>
-                <img src={imagePath} alt={title}/>
-            </div>
+                <div>
+                    <img src={imagePath} alt={title}/>
+                </div>
 
-            <div>
-                <span>{autor}</span>
-                <h3>{title}</h3>
+                <div>
+                    <span>{autor}</span>
+                    <h3>{title}</h3>
+                </div>
+                {/*<Popup trigger={} position="right center">*/}
+                {/*    <div>Popup content here !!</div>*/}
+                {/*</Popup>*/}
             </div>
-            {/*<Popup trigger={} position="right center">*/}
-            {/*    <div>Popup content here !!</div>*/}
-            {/*</Popup>*/}
+            <div>
+                <button type="button" className="button" onClick={() => setOpen(o => !o)}>
+                    Controlled Popup
+                </button>
+                <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+                    <div className="modal">
+                        <a className="close" onClick={closeModal}>
+                            &times;
+                        </a>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae magni
+                        omnis delectus nemo, maxime molestiae dolorem numquam mollitia, voluptate
+                        ea, accusamus excepturi deleniti ratione sapiente! Laudantium, aperiam
+                        doloribus. Odit, aut.
+                    </div>
+                </Popup>
+            </div>
         </div>
 
 
