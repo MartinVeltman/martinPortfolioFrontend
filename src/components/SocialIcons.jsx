0b90fe08e6a29504256b2a318f8029ef48f78fdf
MartinVeltman <martinveltman@live.nl>
2022-06-27 19:19:59 +0200

    updated about me page

---
+++ src/components/SocialIcons.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+import { ExternalLink } from 'react-external-link';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import '../App.css';
+
+const SocialIcons = () => {
+
+    const socialIcons = [
+        {
+            icon: <GitHubIcon />,
+            href: 'https://github.com/MartinVeltman',
+        },
+        {
+            icon: <InstagramIcon />,
+            href: 'https://twitter.com/mupa_mmbetsa',
+        },
+        {
+            icon: <LinkedInIcon />,
+            href: 'https://www.linkedin.com/in/martin-veltman/',
+        },
+    ];
+
+    const getSocialIcons = () => socialIcons.map(({ icon, href }) => (
+        <span key={uuidv4()}>
+            <ExternalLink href={href} className="ico">
+                {icon}
+            </ExternalLink>
+        </span>
+    ));
+
+    return (
+        <>
+            <h3>LETS CONNECT</h3>
+            <span className="erik">{getSocialIcons()}</span>
+        </>
+    );
+};
+
+SocialIcons.defaultProps = {
+    props: () => { },
+};
+
+SocialIcons.propTypes = {
+    props: PropTypes.func,
+};
+
+export default SocialIcons;
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,15 +1,36 @@
 import styled from 'styled-components';
-import {useState} from "react";
 import '../App.css';
+import SocialIcons from "../components/SocialIcons";
 
 const StyledSpan = styled.h1`
   font-weight: bolder;
-  font-size: 1.5em;;
+  font-size: 1.5em;
 
-`
+`;
 
 const StyledH1 = styled.h1`
   font-size: 20px`
+
+const ListItem = styled.li`
+  color: #D88769;`;
+
+const AboutText = styled.p`
+    width: 30%;
+  @media (max-width: 750px){
+    
+  }
+`;
+
+const LanguageList = styled.ul`
+`;
+
+const AboutMeDiv = styled.div`
+    align-items: normal;
+`;
+
+const NavToProjects = styled.button`
+    width: 300px;
+`;
 
 
 function AboutMe() {
@@ -19,17 +40,29 @@ function AboutMe() {
 
 
     return (
-        <div className="app">
+        <AboutMeDiv className="app">
             <StyledH1>My name is <StyledSpan>Martin Veltman</StyledSpan></StyledH1>
-            <p>I am a full stack developer with a passion for learning and creating. I have a background in web
+            <AboutText>I am a full stack developer with a passion for learning and creating. I have a background in web
                 development and have worked in the industry for over 1 year. I have a passion for learning new
                 technologies and I am always looking to learn new things. I am currently in my third year of my bachelor
-                in computer sience</p>
+                in computer sience</AboutText>
 
-            <button onClick={navigateToProjects}>View my projects</button>
+            <StyledH1>Technologies I've learned from best to worst</StyledH1>
+            <LanguageList>
+                <ListItem>Typescript and Javascript</ListItem>
+                <ListItem>Java</ListItem>
+                <ListItem>Angular</ListItem>
+                <ListItem>Springboot</ListItem>
+                <ListItem>React</ListItem>
+                <ListItem>SQL Databases</ListItem>
+                <ListItem>Machine Learning with Python</ListItem>
+                <ListItem>AWS</ListItem>
+                <ListItem>Firebase</ListItem>
+            </LanguageList>
+            <SocialIcons></SocialIcons>
 
-
-        </div>
+            <NavToProjects onClick={navigateToProjects}>View my projects</NavToProjects>
+        </AboutMeDiv>
 
 
     );
